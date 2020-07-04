99f5fb638861037fc90cf8986f4f2169eafad6d2
刘跃龙 <yjkhtddx@sina.com>
2020-07-04 10:21:17 +0800

    wasm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WorkerTestComponent } from './worker-test/worker-test.component';
+import { CanvasTestComponent } from './canvas-test/canvas-test.component';
+import { WasmTestComponent } from './wasm-test/wasm-test.component';
 
 const routes: Routes = [
-  { path: 'worker', component: WorkerTestComponent }
+  { path: 'worker', component: WorkerTestComponent },
+  { path: 'canvas', component: CanvasTestComponent },
+  { path: 'wasm', component: WasmTestComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WorkerTestComponent } from './worker-test/worker-test.component';
+import { CanvasTestComponent } from './canvas-test/canvas-test.component';
+import { WasmTestComponent } from './wasm-test/wasm-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WorkerTestComponent,
+    CanvasTestComponent,
+    WasmTestComponent,
   ],
   imports: [
     BrowserModule,
